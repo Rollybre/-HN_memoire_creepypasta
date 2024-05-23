01b2ffa0a397a8b99922aa316b6f2685d05cc950
Lex <alexandre.lionnet@chartes.psl.eu>
2024-05-23 23:33:00 +0200

    computation tf_idf et emotions

---
+++ 17.01_Python/script/jaccard_distances.py
@@ -0,0 +1,35 @@
+import multiprocessing as mp
+from functools import partial
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+def calculate_jaccard_distances(sample_X, X):
+    combined_X = np.vstack([sample_X, X])
+    distances = squareform(pdist(combined_X, metric='jaccard'))
+    return distances[len(sample_X):, :len(sample_X)]
+
+def parallelize_jaccard(sample, X):
+    combined_X = np.vstack([sample, X])
+    sample_distances = pdist(combined_X, metric='jaccard')[1:]
+    return sample_distances
+
+if __name__ == '__main__':
+    
+    # sample_X = ...
+    # remaining_X = ...
+
+    num_cores = mp.cpu_count()  # Nombre de cœurs disponibles
+    pool = mp.Pool(num_cores)  # Créer un pool de processus
+
+    jaccard_distances = []
+    func = partial(parallelize_jaccard, X=remaining_X)
+    for distance in pool.map(func, sample_X):
+        jaccard_distances.append(distance)
+
+    pool.close()
+    pool.join()
+
+    jaccard_distances = np.array(jaccard_distances)
+
+    # Faites quelque chose avec jaccard_distances
+    # ...
